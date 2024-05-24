b04123569439fa8a3bba5abd6c43f3fb20ca8ffd
AimceptionGian <67605487+AimceptionGian@users.noreply.github.com>
2024-05-24 14:57:11 +0200

    bugfixes

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,10 +4,12 @@ import { ReactComponent as Erklaerung} from '../images/erklaerung.svg';
 function Info() {
     return(
         <div className='Info'>
-            <h6>Leistungen:
-Rechtsschutz: z. B. Durchsetzung von Ansprüchen bei einem Identitätsmissbrauch
-Erstattung finanzieller Schäden: z. B. nach einem Kreditkartenmissbrauch
-Online-Portal mit Cyber-Präventionsservices: z. B. Monitoring von Daten-Diebstählen</h6>
+            <h6>Durch ein Datenleck bei einem Online-Shop wurden Millionen Kundenkonten einschliesslich E-Mail-Adressen gestohlen. Kriminelle haben mit den Accounts bereits Waren bestellt.
+
+Unser Onlinedienst informiert Sie, dass Ihre E-Mail-Adresse von diesem Datendiebstahl betroffen ist, und empfiehlt, das Passwort zu ändern.
+Unsere Anwältinnen und Anwälte unterstützen Sie bei der Durchsetzung ihrer Ansprüche gegenüber den Kriminellen sowie gegenüber dem Shop.
+Zudem übernimmt die AXA allfällige Geldschäden, die am Ende noch bestehen.
+Ihr Selbstbehalt bei finanziellen Schäden beträgt CHF 200, beim Rechtsschutz haben Sie keinen Selbstbehalt.</h6>
             <Erklaerung className='Erklaerung' />
         </div>
     );
