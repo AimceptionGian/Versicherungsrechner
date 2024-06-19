ac80a28dd30fd967d0592375a005abed4865934d
AimceptionGian <67605487+AimceptionGian@users.noreply.github.com>
2024-06-19 08:57:50 +0200

    QuestionProgress überarbeitet

--- src/components/QuestionProgress.js
+++ src/components/QuestionProgress.js
@@ -3,26 +3,49 @@ import { ReactComponent as Adults } from '../images/adults.svg';
 import { ReactComponent as Baby } from '../images/baby.svg';
 import { ReactComponent as Rathaus } from '../images/rathaus.svg';
 import { ReactComponent as Famous } from '../images/famous.svg';
+import { useState, useEffect } from 'react';
 
-const QuestionProgress = ({ progressBarValue1, progressBarValue2, progressBarValue3 }) => {
+const QuestionProgress = ({ progressBarValue1, progressBarValue2, progressBarValue3, questionCounter }) => {
+    const [progressClass1, setProgressClass1] = useState('');
+    const [progressClass2, setProgressClass2] = useState('');
+    const [progressClass3, setProgressClass3] = useState('');
+    const [progressClass4, setProgressClass4] = useState('');
+
+    useEffect(() => {
+        if (questionCounter === 0) {
+            setProgressClass1('InProgress');
+        } else if (questionCounter === 1) {
+            setProgressClass1('Done');
+            setProgressClass2('InProgress');
+        } else if (questionCounter === 2) {
+            setProgressClass2('Done');
+            setProgressClass3('InProgress');
+        } else if (questionCounter === 3) {
+            setProgressClass3('Done');
+            setProgressClass4('InProgress');
+        } else {
+            setProgressClass4('Done');
+        }
+    }, [questionCounter]);
+
     return (
         <div className='QuestionProgress'>
-            <div className='QuestionProgressItem'>
+            <div className={`QuestionProgressItem ${progressClass1}`}>
                 <md-icon-button><Adults className='Adults' /></md-icon-button>
                 <h4>Erwachsene</h4>
             </div>
             <md-linear-progress value={progressBarValue1}></md-linear-progress>
-            <div className='QuestionProgressItem'>
+            <div className={`QuestionProgressItem ${progressClass2}`}>
                 <md-icon-button><Baby className='Baby' /></md-icon-button>
                 <h4>Kinder</h4>
             </div>
             <md-linear-progress value={progressBarValue2}></md-linear-progress>
-            <div className='QuestionProgressItem'>
+            <div className={`QuestionProgressItem ${progressClass3}`}>
                 <md-icon-button><Rathaus className='Rathaus' /></md-icon-button>
                 <h4>Tätigkeit</h4> 
             </div>
             <md-linear-progress value={progressBarValue3}></md-linear-progress>
-            <div className='QuestionProgressItem'>
+            <div className={`QuestionProgressItem ${progressClass4}`}>
                 <md-icon-button><Famous className='Famous' /></md-icon-button>
                 <h4>Social Media</h4> 
             </div>
--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -8,11 +8,23 @@ import QuestionProgress from '../components/QuestionProgress';
 function Questions() {
     const [questionCounter, setQuestionCounter] = useState(0);
     const [questionProgress, setQuestionProgress] = useState(0);
+    const [progress1, setProgress1] = useState(0.5);
+    const [progress2, setProgress2] = useState(0);
+    const [progress3, setProgress3] = useState(0);
 
     function incrementQuestionCounter() {
         setQuestionCounter(prevCounter => {
             const newCounter = prevCounter + 1;
             const newProgress = (newCounter * 25) / 100;
+            if (newCounter === 1) {
+                setProgress1(1);
+                setProgress2(0.5);
+            } else if (newCounter === 2) {
+                setProgress2(1);
+                setProgress3(0.5);
+            } else {
+                setProgress3(1);
+            }
             setQuestionProgress(newProgress);
             return newCounter;
         });
@@ -25,7 +37,7 @@ function Questions() {
                 <Progress progressBarValue1='1' progressBarValue2={questionProgress}/>
                 <div className='QuestionProgressContainer'>
                     <md-divider />
-                    <QuestionProgress />
+                    <QuestionProgress progressBarValue1={progress1} progressBarValue2={progress2} progressBarValue3={progress3} questionCounter={questionCounter}/>
                 </div>
             </header>
             <div className='Content'>
